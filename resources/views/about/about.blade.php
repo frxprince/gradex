b964d8e4cd513d69a3ae3f9eb67e7a78093f86f0
Drpaween <frxprince@gmail.com>
2019-08-01 13:30:58 +0700

    add about page

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -5,7 +5,7 @@
     <p class="lead">Version 2.0</p>
     <hr class="my-4">
     <p>This software is developed by Miss Kanyarat	Kerdphon and Miss Boonyanuch Sukala as a senoir project,
-        under supervision of Asst. Prof. Kongkarn Dullayachai and Dr. Part Pramokchon and Dr. Paween Khoenkaw<br/>
+        under supervision of Asst. Prof. Kongkarn Dullayachai, Dr. Part Pramokchon and Dr. Paween Khoenkaw<br/>
     The codebase is maintained by Dr.Paween Khoenkaw, please report bugs and problems directly to email:paween_k@gmaejo.mju.ac.th
     </p>
 </div>
